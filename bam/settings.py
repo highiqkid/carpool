aa70c9cf6d461e016dc9c6b3a5f57247663d4bd2
highiqkid <victor_cheng@yahoo.com>
2016-09-03 21:30:58 -0700

    heroku changes

--- bam/settings.py
+++ bam/settings.py
@@ -125,6 +125,16 @@ USE_L10N = True
 USE_TZ = True
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 FACEBOOK_APP_ID='245141129215287'
